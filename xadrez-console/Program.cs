491eac8611367f6a0153bb1c4c820495792f8c8c
Giovani Santos Cardoso <gioos.cardoso@gamil.com>
2022-12-25 23:39:50 -0300

    Implementando o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -2,7 +2,9 @@
 
 using tabuleiro;
 
-Posicao P;
-P = new Posicao(3, 4);
-Console.WriteLine("Posição: " + P);
-Console.ReadLine();
+Tabuleiro t = new Tabuleiro(8, 8);
+
+    Console.ReadLine();
+
+
+
